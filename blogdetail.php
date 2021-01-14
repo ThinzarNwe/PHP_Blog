6aee1e22e8c23381c3ebb0c73420470d951d2aee
Thinzar Nwe <thinzar167496@gmail.com>
2021-01-15 00:29:55 +0630

    first commit

---
+++ blogdetail.php
@@ -0,0 +1,155 @@
+<?php
+session_start();
+require 'config/config.php';
+
+
+if(empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])){
+  header('Location: login.php');
+}
+
+  $stmt = $pdo->prepare("SELECT * FROM posts WHERE id=".$_GET['id']);
+  $stmt->execute();
+  $result = $stmt->fetchAll();
+
+  $blogID = $_GET['id'];
+
+  $stmtcmt = $pdo->prepare("SELECT * FROM comments WHERE post_id=$blogID");
+  $stmtcmt->execute();
+  $cmResult = $stmtcmt->fetchAll();
+
+  $authorId = $cmResult[0]['author_id']; 
+  $stmtau = $pdo->prepare("SELECT * FROM users WHERE id=$authorId");
+  $stmtau->execute();
+  $auResult = $stmtau->fetchAll();
+ 
+  if($_POST){
+    $comment = $_POST['comment'];
+    $stmt = $pdo->prepare("INSERT INTO comments(content,author_id,post_id) VALUES (:content,:author_id,:post_id)");
+    $result = $stmt->execute(
+      array(':content'=>$comment,':author_id'=>$_SESSION['user_id'],':post_id'=>$blogID)
+        );
+        if($result){
+          header('Location: blogdetail.php?id='.$blogID);
+        
+    }
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 3 | Widgets</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" style="margin-left: 0px !important">
+    
+
+    <!-- Main content -->
+    <section class="content">
+       <div class="row">
+          <div class="col-md-12">
+            <!-- Box Comment -->
+            <div class="card card-widget">
+              <div class="card-header">
+                <!-- /.user-block -->
+                <div style="text-align: center; !important;float:none;" class="card-title">
+                  <h4><?php echo $result[0]['title'] ?></h4>
+                </div>
+                <!-- /.card-tools -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+               <img src="admin/images/<?php echo $result[0]['image'] ?>"class="container-fluid" style="height: 850px !important;">
+
+                <p><?php echo $result[0]['content'] ?></p>   
+                <h3>Comments</h3>   
+                <a href="/admin" type="button" class="btn btn-default">Go Back</a>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer card-comments">
+                <div class="card-comment">
+
+                  <div class="comment-text" style="margin-left: 0px !important">
+                    <span class="username">
+                      <?php   echo $auResult[0]['name']; ?>
+                      <span class="text-muted float-right"><?php   echo $cmResult[0]['created_id']; ?></span>
+                    </span><!-- /.username -->
+                    <?php   echo $cmResult[0]['content']; ?>
+                  </div>
+                  <!-- /.comment-text -->
+                </div>
+                
+              </div>
+              <!-- /.card-footer -->
+              <div class="card-footer">
+                <form action="" method="post">
+                  <!-- .img-push is used to add margin to elements next to floating images -->
+                  <div class="img-push">
+                    <input type="text" name="comment" class="form-control form-control-sm" placeholder="Press enter to post comment">
+                  </div>
+                </form>
+              </div>
+              <!-- /.card-footer -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+    </section>
+
+    <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+    <!-- /.content -->
+
+    <a id="back-to-top" href="#" class="btn btn-primary back-to-top" role="button" aria-label="Scroll to top">
+      <i class="fas fa-chevron-up"></i>
+    </a>
+  </div>
+  <!-- /.content-wrapper -->
+
+    <footer class="main-footer" style="margin-left: 0px !important">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      <a href="logout.php" type="button" class="btn btn-default">Logout</a>
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020-2021 <a href="https://adminlte.io">ThinzarNwe</a>.</strong> All rights reserved.
+  </footer>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,151 @@
+<?php
+session_start();
+require 'config/config.php';
+
+if(empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])){
+  header('Location: login.php');
+}
+
+                if (!empty($_GET['pageno'])){
+                  $pageno = $_GET['pageno'];
+                }else{
+                  $pageno = 1;
+                }
+                $numOfrecs = 6;
+                $offset = ($pageno - 1)* $numOfrecs;
+
+               if(empty($_POST['search'])) {
+                  $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC");
+                  $stmt->execute();
+                  $rawResult = $stmt->fetchAll();
+                  $total_pages = ceil(count($rawResult) / $numOfrecs);
+
+                  $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC LIMIT $offset,$numOfrecs");
+                  $stmt->execute();
+                  $result = $stmt->fetchAll();
+               }else{
+                  $searchKey = $_POST['search'];
+                  $stmt = $pdo->prepare("SELECT * FROM posts WHERE title LIKE '%searchKey' ORDER BY id DESC");
+                  $stmt->execute();
+                  $result = $stmt->fetchAll();
+
+                  $total_pages = ceil(count($result) / $numOfrecs);
+
+                  $stmt = $pdo->prepare("SELECT * FROM posts WHERE title LIKE '%$searchKey%' ORDER BY id DESC LIMIT $offset,$numOfrecs");
+                  $stmt->execute();
+                  $result = $stmt->fetchAll();
+               }
+
+               
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 3 | Widgets</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="">
+    <!-- Content Header (Page header) -->
+    <section class="content-header" >
+      <div class="container-fluid">
+       <h1 style="text-align: center;">Widgets</h1>
+      </div><!-- /.container-fluid -->
+    </section>
+    <?php   
+      $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC");
+      $stmt->execute();
+      $result = $stmt->fetchAll();
+    ?>
+    <!-- Main content -->
+    <section class="content">
+       <div class="row">
+         <?php 
+          if ($result) {
+            $i = 1;
+            foreach ($result as $value) { ?>
+              <div class="col-md-4">
+            <!-- Box Comment -->
+                <div class="card card-widget">
+                  <div class="card-header">
+                    <div style="text-align: center; !important;float:none;" class="card-title">
+                  <h4><?php echo $value['title'] ?></h4>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <a href=" blogdetail.php?id=<?php echo $value['id'];?>"><img src="admin/images/<?php echo $value['image'] ?>"class="container-fluid" style="height: 350px  !important;"></a>
+                
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+
+                  <?php 
+                      $i++;
+                    }
+                  }
+                  ?>
+          
+        </div>
+    </section>
+     <nav aria-label="Page navigation example" style="float: right;">
+        <ul class="pagination">
+          <li class="page-item"><a class="page-link" href="?pageno=1">First</a></li>
+          <li class="page-item <?php if($pageno <= 1){echo 'disabled';} ?>"><a class="page-link" href="<?php if($pageno <= 1){echo'#';}else{echo "?pageno=".($pageno-1);} ?>">Previous</a></li>
+          <li class="page-item"><a class="page-link" href="#"><?php echo $pageno; ?></a></li>
+          <li class="page-item <?php if($pageno >= $total_pages){echo 'disable';} ?> "><a class="page-link" href="<?php if($pageno >= $total_pages){echo '#';}else{echo"?pageno=".($pageno+1);} ?>">Next</a></li>
+          <li class="page-item"><a class="page-link" href="?pageno=<?php echo $total_pages?>">Last</a></li>
+        </ul>
+      </nav>
+    <!-- /.content -->
+
+    <a id="back-to-top" href="#" class="btn btn-primary back-to-top" role="button" aria-label="Scroll to top">
+      <i class="fas fa-chevron-up"></i>
+    </a>
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer" style="margin-left: 0px !important">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      <a href="logout.php" type="button" class="btn btn-default">Logout</a>
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020-2021 <a href="https://adminlte.io">ThinzarNwe</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+</body>
+</html>
+
+
