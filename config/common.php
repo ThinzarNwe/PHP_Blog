dbd961dad09b983fa3789d6455e53ed060395bcf
Thinzar Nwe <thinzar167496@gmail.com>
2021-01-23 09:50:31 +0630

    Fix error

--- config/common.php
+++ config/common.php
@@ -1,6 +1,15 @@
 <?php
 
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (!hash_equals($_SESSION['_token'], $_POST['_token'])){
+    echo 'Invalid CSRF token';
+    die();
+  }else{
+    unset($_SESSION['_token']);
+  }
+}
+
 if (empty($_SESSION['_token'])) {
 	if (function_exists('random_bytes')) {
 		$_SESSION['_token'] = bin2hex(random_bytes(32));
@@ -12,14 +21,6 @@ if (empty($_SESSION['_token'])) {
 }
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  if (!hash_equals($_SESSION['_token'], $_POST['_token'])){
-    echo 'Invalid CSRF token';
-    die();
-  }else{
-    unset($_SESSION['_token']);
-  }
-}
 
 
 
